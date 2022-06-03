f109387ff4fbfce41d82ee6d631926184195bb94
Abysinia <abysinia@gmail.com>
2022-06-03 16:46:29 -0700

    add

--- 0x01-variables_if_else_while/1-last_digit.cc
+++ 0x01-variables_if_else_while/1-last_digit.cc
@@ -2,9 +2,10 @@
 #include<time.h>
 #include<stdio.h>
 /**
- * main - Prints a text according number
- * Return: Always (Success)
- */
+ * * main - Prints a text according number
+ * *
+ * * Return: Always (Success)
+ * */
 
 int main(void)
 {
