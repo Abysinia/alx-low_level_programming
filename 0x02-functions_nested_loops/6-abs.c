82d8ffaf1f880cda34f18dd6225e15f2c37a2dd9
Abysinia <abysinia@gmail.com>
2022-06-07 18:33:56 -0700

    add _abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _abs - absolute balue of the number from 0
+ * @i: the integer to be covered.
+ * Return: absolute balue of the input #
+ */
+int _abs(int i)
+{
+if (i < 0)
+i = i * -1;
+return (i);
+}
