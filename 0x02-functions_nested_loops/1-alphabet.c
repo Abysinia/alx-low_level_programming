01240cbdd16d077c3d83a4e304adab3b23e9bc57
Abysinia <abysinia@gmail.com>
2022-06-07 17:03:40 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *  print alphabet -prints the english alphabet from a-z
+ *  Return: Nothing.
+ */
+void print_alphabet(void)
+{
+
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++0)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
