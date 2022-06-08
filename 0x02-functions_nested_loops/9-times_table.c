57108fe621e20f1022669e4c42b2a56d9dbbd032
Abysinia <abysinia@gmail.com>
2022-06-07 19:32:06 -0700

    add time table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,26 +9,26 @@ int timerow, timecol, timedigit;
 
 for (timerow = 0; timerow <= 9; timerow++)
 {
-_putchar('0');
-_putchar('1');
-_putchar(' ');
 for (timecol = 1; timecol <= 9; timecol++)
 {
 timedigit = (timerow * timecol);
-if ((timedigit / 10) > 0)
+if (timedigit == 0)
 {
-_putchar((timedigit / 10) + '0');
+_putchar(timedigit + '0');
 }
-else
-{
-_putchar(' ');
-}
-_putchar((timedigit % 10) + '0');
-
-if (timecol < 9)
+else if (timedigit <= 9)
 {
 _putchar(',');
 _putchar(' ');
+_putchar(' ');
+_putchar(timedigit + '0');
+}
+else if (timecol >= 10)
+{
+_putchar(',');
+_putchar(' ');
+_putchar(timedigit / 10 + '0');
+_putchar(timedigit % 10 + '0');
 }
 }
 _putchar('\n');
