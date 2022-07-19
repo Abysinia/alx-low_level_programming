77624ad1911ab9ee4612720d37e8b03936de17a4
Abysinia <abysinia@gmail.com>
2022-07-19 15:44:32 -0700

    update 0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,6 +1,5 @@
 #include "lists.h"
 #include <stdio.h>
-
 /**
  * print_listint - print elements of linkedlist
  * @h: head
@@ -9,15 +8,13 @@
 
 size_t print_listint(const listint_t *h)
 {
-	size_t no_nodes = 0;
-
-	const listint_t *currentv = h;
-
-	while (currentv != NULL)
-	{
-		printf("%i\n", currentv->n);
-		currentv = currentv->next;
-		no_nodes++;
-	}
-	return (no_nodes);
+size_t no_nodes = 0;
+const listint_t *currentv = h;
+while (currentv != NULL)
+{
+printf("%i\n", currentv->n);
+currentv = currentv->next;
+no_nodes++;
+}
+return (no_nodes);
 }
